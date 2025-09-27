e60532fb05231eca127aba5b86b9e180b7b47299
Kemuel Áquila de Matos <kemueldematos.18@gmail.com>
2025-09-27 20:47:52 -0300

    Create Plano.swift

---
+++ Gym/Plano.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+class Plano {
+    var nome: String
+
+    init(nome: String) {
+        self.nome = nome
+    }
+
+    func calcularMensalidade() -> Double {
+        return 0.0
+    }
+}
