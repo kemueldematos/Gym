335d6797e66185f72f66bca8c45f672a9736561e
Kemuel Áquila de Matos <kemueldematos.18@gmail.com>
2025-09-27 20:47:35 -0300

    Create Pessoa.swift

---
+++ Gym/Pessoa.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+class Pessoa {
+    var nome: String
+    var email: String
+
+    init(nome: String, email: String) {
+        self.nome = nome
+        self.email = email
+    }
+
+    func getDescricao() -> String {
+        return "Nome: \(self.nome) / Email: \(self.email)"
+    }
+}
