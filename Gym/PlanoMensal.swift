0db609d987ceebbd89ba9c0994b31e75dfe521ee
Kemuel Áquila de Matos <kemueldematos.18@gmail.com>
2025-09-27 20:49:31 -0300

    Create PlanoMensal.swift

---
+++ Gym/PlanoMensal.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+class PlanoMensal: Plano {
+    init() {
+        super.init(nome: "Plano Mensal")
+    }
+
+    override func calcularMensalidade() -> Double {
+        return 120.0
+    }
+}
