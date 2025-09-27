acb17860c4cacc5913c630e0abf33fe9c0533c10
Kemuel Áquila de Matos <kemueldematos.18@gmail.com>
2025-09-27 20:45:40 -0300

    Create Aula.swift

---
+++ Gym/Aula.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+class Aula {
+    var nome: String
+    var instrutor: Instrutor
+
+    init(nome: String, instrutor: Instrutor) {
+        self.nome = nome
+        self.instrutor = instrutor
+    }
+
+    func getDescricao() -> String {
+        return "Aula: \(self.nome) / Instrutor: \(self.instrutor.nome)"
+    }
+}
